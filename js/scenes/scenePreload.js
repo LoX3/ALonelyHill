99cef0587270bc8b46a62b2ea8bb77a09ce9b227
TheJltres <TheJltres@bitbucket.org>
2019-12-17 15:57:45 +0100

    Añadido escena de preload

---
+++ js/scenes/scenePreload.js
@@ -0,0 +1,54 @@
+/**
+ * @class Se cargan los assets del juego
+ */
+class ScenePreload extends Phaser.Scene {
+    /**
+     * @constructor Constructor de la escena
+     */
+    constructor() {
+        super(
+            {
+                key: 'ScenePreload',
+            }
+        )
+    }
+
+    /**
+     * @function init Init es la primera función que se llama cuando se
+     * inicia su estado. Se llama antes de precargar, crear o cualquier
+     * otra cosa. Si necesita enrutar el juego a otro estado, puede
+     * hacerlo aquí, o si necesita preparar un conjunto de variables u
+     * objetos antes de que comience la precarga.
+     */
+    init() {
+
+    }
+
+    /**
+     * @function preload Preload se llama primero. Normalmente usaría
+     * esto para cargar sus activos de juego (o los necesarios para el
+     * Estado actual). No debe crear ningún objeto en este método que
+     * requiera activos que también esté cargando en este método, ya
+     * que aún no serán disponible.
+     */
+    preload() {
+
+    }
+
+    /**
+     * @function create Create sellama una vez que se ha completado la
+     * precarga. Si no tiene un método de precarga, crear es el primer
+     * método llamado en su estado.
+     */
+    create() {
+
+    }
+
+    /**
+     * @function update Update se llama una vez finalizado la carga de los 
+     * archivos para poder jugar
+     */
+    update() {
+
+    }
+}
--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 // No van los import!
 // Mas exactamente no van con express i soket.io
+
+/**
+ * @file Aquí se ejecuta el servidor para poder juegar a nuestro videojuego
+ */
+
+/**
+ * @typedef { require('express') } app
+ */
 var express = require('express');
 var app = express();
+
+/**
+ * @typedef { require('http').Server(app) } server
+ */
 var server = require('http').Server(app);
+
+/**
+ * @typedef { require('socket.io') } io
+ */
 var io = require('socket.io').listen(server);
 
-app.use('/js/class', express.static(__dirname + '/js/class'));
-app.use('/js/phaser', express.static(__dirname + '/js/phaser'));
-app.use('/js/scenes', express.static(__dirname + '/js/scenes'));
-app.use('/js/scenes', express.static(__dirname + '/js/scenes'));
+/**
+ * Load folders
+ */
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
+/**
+ * Load HTML file
+ */
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/html/index.html');
 });
 
+/**
+ * Get up the server on port 8080
+ */
 server.listen(process.env.PORT || 8080, function () {
     console.log('Listening on ' + server.address().port);
 });
