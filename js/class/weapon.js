02f2202cf78eea7792cc86af0571b4659c864734
TheJltres <TheJltres@bitbucket.org>
2019-12-17 17:31:33 +0100

    Añadidos clases i enums

---
+++ js/class/weapon.js
@@ -0,0 +1,50 @@
+/**
+ * @class Weapon - Arma del jugador
+ */
+class Weapon extends Phaser.GameObjects.Container {
+    /**
+     * @constructor Constructor del arma del jugador
+     */
+    constructor(butt, body, canon) {
+        this.addAt
+    }
+
+    /**
+     * @function init Init es la primera función que se llama cuando se
+     * inicia su estado. Se llama antes de precargar, crear o cualquier
+     * otra cosa. Si necesita enrutar el juego a otro estado, puede
+     * hacerlo aquí, o si necesita preparar un conjunto de variables u
+     * objetos antes de que comience la precarga.
+     */
+    init() {
+
+    }
+
+    /**
+     * @function preload Preload se llama primero. Normalmente usaría
+     * esto para cargar sus activos de juego (o los necesarios para el
+     * Estado actual). No debe crear ningún objeto en este método que
+     * requiera activos que también esté cargando en este método, ya
+     * que aún no serán disponible.
+     */
+    preload() {
+
+    }
+
+    /**
+     * @function create Create sellama una vez que se ha completado la
+     * precarga. Si no tiene un método de precarga, crear es el primer
+     * método llamado en su estado.
+     */
+    create() {
+
+    }
+
+    /**
+     * @function update Update se llama una vez finalizado la carga de los 
+     * archivos para poder jugar
+     */
+    update() {
+
+    }
+}
